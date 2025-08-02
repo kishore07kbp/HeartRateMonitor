dcc71269dcfa419596de144a955e1aaf649307ba
Kishore T <155010499+kishore07kbp@users.noreply.github.com>
2025-08-02 13:35:04 +0530

    Update WebConfig.java

--- HRM-Backend/src/main/java/com/example/Heart_Rate_Monitor/config/WebConfig.java
+++ HRM-Backend/src/main/java/com/example/Heart_Rate_Monitor/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://heart-rate-monitor-omega.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
